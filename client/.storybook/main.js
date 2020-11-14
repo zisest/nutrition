83c271b454933afca414e19d52c7f85051457386
42969365 <zisest@gmail.com>
2020-11-14 12:21:07 +1000

    Bump react from 16.13.1 to 17.0.1
    bump react-scripts from 3.4.1 to 4.0.0
    bump storybook from 6.0.28 to 6.1.0-rc.2
    add svgr loader rule to storybook webpack config

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -5,5 +5,15 @@ module.exports = {
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links'
-  ]
+  ],
+  webpackFinal: async (config) =>  {
+    // remove svg from existing rule, add svgr rule
+    const fileLoaderRule = config.module.rules.find(rule => rule.test.test('.svg'))
+    fileLoaderRule.exclude = /\.svg$/
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack", "url-loader"],
+    })
+    return config
+  }
 }
